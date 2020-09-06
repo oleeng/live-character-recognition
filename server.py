71da85e8e309fb264fb60f06cda208848a8e9a44
Ole <ole_e@web.de>
2020-09-06 14:36:08 +0200

    fixed flask for heroku

--- server.py
+++ server.py
@@ -44,4 +44,6 @@ def recognize():
 
     return json.dumps(response)
 
-app.run()
+if __name__ == '__main__':
+    # Threaded option to enable multiple instances for multiple user access support
+    app.run(threaded=True, port=5000)
